518ac5c0fab58cb29b4046ace41096e86708a0ce
Joulster <shobhit.chitkara@gmail.com>
2020-05-12 19:07:35 +0530

    Added correct file path

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,19 @@
-filepath <- file.path(getwd(),"R","Test Material","PA 4") # Directory where dataset is downloaded, unzipped and saved
-setwd(filepath) # Set WD to appropriate location  
+#Download file from data source
+if(!file.exists("./data")){dir.create("./data")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, destfile = "./data/UCIMLDataset.zip")
+#Unzip file into a local directory
+unzip(zipfile = "./data/UCIMLDataset.zip",exdir = "./data") 
 # Read test & train datasets into R
-x_test <- read.delim("./UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
-y_test <- read.delim("./UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
-subject_test <- read.delim("./UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
-x_train <- read.delim("./UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
-y_train <- read.delim("./UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
-subject_train <- read.delim("./UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
+x_test <- read.delim(".data/UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
+y_test <- read.delim(".data/UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
+subject_test <- read.delim(".data/UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
+x_train <- read.delim(".data/UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
+y_train <- read.delim(".data/UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
+subject_train <- read.delim(".data/UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
 # Read features and activity labels into R to define column names
-features <- read.delim("./UCI HAR Dataset/features.txt", sep = "", header = FALSE)
-activity_labels <- read.delim("./UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
+features <- read.delim(".data/UCI HAR Dataset/features.txt", sep = "", header = FALSE)
+activity_labels <- read.delim(".data/UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
 names(activity_labels) <- c("activity_id","activity_name")
 #merge activity and corresponding dataset
 combined_trainset <- cbind(subject_train,y_train,x_train)
